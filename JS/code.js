efb507190a2bf3bd5142004f81a9e44988586795
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-08-25 03:52:35 +0200

    ...Fixing Dropdown 😪

--- JS/code.js
+++ JS/code.js
@@ -23,30 +23,28 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
+    origin: "top",
+    distance: "80px",
     duration: 2000,
     reset: true,
 });
 
-
-
-sr.reveal('.Home__Title', {});
+sr.reveal(".Home__Title", { delay: 200 });
 sr.reveal(".Home__Button__Contact", { delay: 200 });
 sr.reveal(".Home__img", { delay: 400 });
-sr.reveal(".Home__Social__icon", { interval: 200 }); *
+sr.reveal(".Home__Social__icon", { interval: 200 });
 
-sr.reveal('.About__Image', {});
-sr.reveal('.About__Subtitle', { delay: 400 });
-sr.reveal('.About__Title', { delay: 400 });
+sr.reveal(".About__Image", { delay: 200 });
+sr.reveal(".About__Subtitle", { delay: 400 });
+sr.reveal(".About__Title", { delay: 400 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.Skills__Subtitle', {});
-sr.reveal('.Skills__Data', { interval: 200 });
-sr.reveal('.Skills__IMG', { delay: 600 });
+sr.reveal(".Skills__Subtitle", {});
+sr.reveal(".Skills__Data", { interval: 200 });
+sr.reveal(".Skills__IMG", { delay: 600 });
 
 /*SCROLL WORK*/
-sr.reveal('.Work__IMG', { interval: 200 });
+sr.reveal(".Work__IMG", { interval: 200 });
 
 /*SCROLL CONTACT*/
-sr.reveal('.Contact__Input', { interval: 200 });
+sr.reveal(".Contact__Input", { interval: 200 });
